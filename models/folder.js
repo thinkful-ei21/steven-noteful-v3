70efef50c1e25d29df9464992299b71a9701ed07
Steven Imus <imussg@gmail.com>
2018-06-07 14:39:53 -0400

    starting point for many-to-many mongo

---
+++ models/folder.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const mongoose = require('mongoose');
+
+const folderSchema = new mongoose.Schema({
+	name: { type: String, required: true, unique: true},
+
+});
+
+folderSchema.set('timestamps', true);
+
+folderSchema.set('toObject', {
+	virtuals: true,
+	versionKey: false,
+	transform: (doc, ret) => {
+		delete ret._id;
+	}
+});
