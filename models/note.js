5370f8d28d3a6b923500b8f93ad9f46e37500ec6
Steven Imus <imussg@gmail.com>
2018-06-07 10:26:03 -0400

    testing solution

--- models/note.js
+++ models/note.js
@@ -2,11 +2,19 @@
 
 const mongoose = require('mongoose');
 
-const notesSchema = mongoose.Schema({
-	title: {type: String, required: true},
-	content: String
+const noteSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  content: String
 });
 
-notesSchema.set('timestamps', true);
+// Add `createdAt` and `updatedAt` fields
+noteSchema.set('timestamps', true);
 
-module.exports = mongoose.model('Note', noteSchema);
+// Customize output for `res.json(data)`, `console.log(data)` etc.
+noteSchema.set('toObject', {
+  virtuals: true,     // include built-in virtual `id`
+  versionKey: false,  // remove `__v` version key
+  transform: (doc, ret) => {
+    delete ret._id; // delete `_id`
+  }
+});
